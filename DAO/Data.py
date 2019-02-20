0843ccff1726aad1fbf97004d83fab1d3db70d71
Ren Dlny <ren.delaney@gmail.com>
2019-02-20 20:04:24 +0000

    folder structure improved

--- DAO/Data.py
+++
@@ -1,8 +0,0 @@
-# Database Details
-DB_USERNAME = ""
-DB_PASSWORD = ""
-DB_HOST = ""
-DB_NAME = ""
-
-
-WEB_LINK = "https://yokaiwatch.fandom.com/wiki/Baffle_Boards"
--- webscrape.py
+++ webscrape.py
@@ -1,11 +1,11 @@
 import urllib.request
 from bs4 import BeautifulSoup
-from DAO import Data
-import functions
+from assets import data
+from assets import functions
 
 from DTO.BaffleBoard import BaffleBoard
 
-page = functions.scrapeUrl(Data.WEB_LINK)
+page = functions.scrapeUrl(data.WEB_LINK)
 tableHead = page.find('span', {"id": "Yo-kai_Watch_3"})
 table = tableHead.find_parent().find_next_sibling()
 tableRows = table.find_all('tr')
@@ -19,15 +19,15 @@ for row in tableRows:
         rowData = row.find_all('td')
         dataCount = 0
 
-        for data in rowData:
+        for dataCol in rowData:
             dataCount = dataCount + 1
 
             ## GET LOCATION
             if dataCount is 1:
-                location = data.text
+                location = dataCol.text
             ## GET CLUES
             if dataCount is 2:
-                clues = data.find_all('li')
+                clues = dataCol.find_all('li')
                 clueCount = 0
                 for clue in clues:
 
@@ -41,15 +41,15 @@ for row in tableRows:
 
             ## GET SOLUTION
             if dataCount is 3:
-                solution = data.text
+                solution = dataCol.text
 
             ## GET EFFECT
             if dataCount is 4:
-                effect = data.text
+                effect = dataCol.text
 
         #print(location)
         #print('Location: ' + location + ', Clue 1: ' + clue_1 + ', Clue 2: ' + clue_2 + ', Clue 3: ' + clue_3 + ', Solution: ' + solution + ', Effect: ' + effect)
         baffleBoard = BaffleBoard( location.rstrip(), clue_1.rstrip(), clue_2.rstrip(), clue_3.rstrip(), solution.rstrip(), effect.rstrip())
 
-        test = functions.addBaffleBoardToFile(baffleBoard, "baffleBoard3.js")
+        test = functions.addBaffleBoardToFile(baffleBoard, "test.js")
         print(test)
